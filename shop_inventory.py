7534a2c6d4c7bb2d75349026c696175cc68a918b
Vishnu <vishnuvishnu28024@gmail.com>
2026-03-14 18:25:34 +0530

    Add files via upload

---
+++ shop_inventory.py
@@ -0,0 +1,22 @@
+print("Shop Inventory : \n")
+
+milk_name = "Aavin"
+milk_price = 30
+milk_quantity = 20
+milk_available = True
+
+rice_name = "Kichili Samba"
+rice_price = 200
+rice_quantity = 50
+rice_available = True
+
+print("milk name :",milk_name)
+print("milk price :",milk_price)
+print("milk quantity :",milk_quantity)
+print("milk available :",milk_available)
+print("\n")
+print("rice name :",rice_name)
+print("rice price :",rice_price)
+print("rice quantity :",rice_quantity)
+print("rice available :",rice_available)
+
