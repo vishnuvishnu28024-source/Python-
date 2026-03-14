de5a5bcd04607f817dab0af1e65fc78cc6e37d86
Vishnu <vishnuvishnu28024@gmail.com>
2026-03-14 18:27:07 +0530

    Add files via upload

---
+++ type_converter.py
@@ -0,0 +1,14 @@
+integer = 28
+decimal = 7.2
+string = "vishnu"
+charactor = 's'
+boolean = True
+
+print("float -> int :", int(decimal))
+print("int -> float :", float(integer))
+print("bool -> int :", int(boolean))
+print("bool -> float :", float(boolean))
+print("string -> bool :", bool(string))
+print("int -> string :", str(integer))
+print("float -> string :", str(decimal))
+print("bool -> string :", str(boolean))
